e79295f9427146300362d5b423ec5f1b7540c414
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-07-26 16:56:21 +0530

    Add files via upload

---
+++ 3.AssignmentOperators.java
@@ -0,0 +1,15 @@
+class Operator 
+{
+    public static void main (String a[])
+    {
+      int z=12;
+      int b=6;
+      int result =z/b;
+
+      result++;
+        System.out.println(result);
+      result--;
+        System.out.println(result);
+
+    }
+}
