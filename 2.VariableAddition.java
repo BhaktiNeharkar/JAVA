d13580e95fdf24c572c9aae5c92542ceedee0cd5
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-07-26 16:32:56 +0530

    Add files via upload

---
+++ 2.VariableAddition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+    public static void main (String a[])
+    {
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2 ;
+      System.out.println(result);
+    }
+}
