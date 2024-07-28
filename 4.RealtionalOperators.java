b9e4c0ebfda5e17e14342cb6a9b9e1f54cf28c63
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-07-28 14:56:52 +0530

    Add files via upload

---
+++ 4.RealtionalOperators.java
@@ -0,0 +1,13 @@
+class Relation 
+{
+    public static void main(String Args[])
+    {
+    int a=10 ;
+    int b=20;
+
+    if(a>=b)
+        System.out.println("Yes");
+    else 
+        System.out.println("No");        
+   }
+}   
