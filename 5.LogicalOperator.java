d1e646d9edb3b14ad8d2b6cf19fde6c531b36779
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-07-30 23:46:24 +0530

    Add files via upload

---
+++ 5.LogicalOperator.java
@@ -0,0 +1,16 @@
+class Logic
+{
+    public static void main (String Args[])
+    {
+        int a=10;
+        int b=20;
+        int x=1;
+        int y=2;
+
+        if(a>b && x<y)
+            System.out.println("Yes");
+        else 
+            System.out.println("No");
+    }
+
+}
