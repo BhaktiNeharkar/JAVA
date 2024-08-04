a0b08b1f23dc506e1a700a38778bcc369465df05
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-08-05 01:02:43 +0530

    Add files via upload

---
+++ 7.Ternary.java
@@ -0,0 +1,11 @@
+class Ternary
+{
+    public static void main (String Args[])
+    {
+        int a=20;
+        int result=0;
+      
+        result= a%2==0? 10 : 20;
+        System.out.println(result);
+    }
+}
