5f4352b7cdfb7c7ed6b345520b9f11fe0a0c845f
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-08-27 23:35:29 +0530

    Add files via upload

---
+++ 9.WhileLoop.java
@@ -0,0 +1,12 @@
+class Whileloop
+{
+    public static void main(String Args[])
+    {
+        int i=0;
+        while(i<=4)
+        {
+            System.out.println("Hello");
+            i++;
+        }
+    }
+}
