ddf0bc0917de7dbc178bca51fe90c058c5713cb5
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2025-01-03 23:58:52 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,11 @@
+//Write a Java program to add two numbers.
+
+class Add {
+
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
